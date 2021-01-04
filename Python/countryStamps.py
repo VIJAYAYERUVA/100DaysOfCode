b1dabad61d04fd2b84531d56648deebeb4e68ec5
Vijaya Kumari Yeruva <25230005+VIJAYAYERUVA@users.noreply.github.com>
2021-01-03 22:28:23 -0600

    Day18
    Day18 of # 100DaysOfCode

---
+++ Python/countryStamps.py
@@ -0,0 +1,9 @@
+# https://www.hackerrank.com/challenges/py-set-add/problem
+
+N = int(input())
+countries = set()
+
+for i in range(N):
+    country = input();
+    countries.add(country)
+print(len(countries))
