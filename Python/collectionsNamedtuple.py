1e115388ed2ee0141ea85b92b10527ba20fc1a93
VIJAYA KUMARI YERUVA <VIJAYAYERUVA@users.noreply.github.com>
2021-01-26 21:01:14 -0600

    Day41 of # 100DaysOfCode

---
+++ Python/collectionsNamedtuple.py
@@ -0,0 +1,12 @@
+# https://www.hackerrank.com/challenges/py-collections-namedtuple/problem
+
+from collections import namedtuple
+
+n = int(input())
+a = input()
+total = 0
+Student = namedtuple('Student', a)
+for _ in range(n):
+    student = Student(*input().split())
+    total += int(student.MARKS)
+print('{:.2f}'.format(total / n))
