41e545dda5134fe9ee6de9f14f092488cea22130
VIJAYA KUMARI YERUVA <VIJAYAYERUVA@users.noreply.github.com>
2021-01-29 00:09:04 -0600

    Day43 of # 100DaysOfCode

--- Python/wordOrder.py
+++ Python/wordOrder.py
@@ -1,1 +1,15 @@
 # https://www.hackerrank.com/challenges/word-order/problem
+
+n = int(input())
+d = {}
+for _ in range(n):
+    k = input()
+    if k in d:
+        d[k] += 1
+    else:
+        d[k] = 1
+
+print(len(d))
+
+for i in d:
+    print(d[i], end=' ')
