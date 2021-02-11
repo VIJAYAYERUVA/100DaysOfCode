1cb30363620448a7299f06f62cade84466fd6936
VIJAYA KUMARI YERUVA <VIJAYAYERUVA@users.noreply.github.com>
2021-02-10 21:02:56 -0600

    Day55 of # 100DaysOfCode

---
+++ Python/sets8.py
@@ -0,0 +1,5 @@
+# https://www.hackerrank.com/challenges/py-check-subset/problem
+
+for _ in range(int(input())):
+    x, A, y, B = input(), set(input().split()), input(), set(input().split())
+print(A.issubset(B))
