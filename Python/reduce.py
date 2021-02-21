035daa12b45f79c78813c1fc6bd3a41f6755cb64
VIJAYA KUMARI YERUVA <VIJAYAYERUVA@users.noreply.github.com>
2021-02-20 23:33:59 -0600

    Day65 of # 100DaysOfCode

---
+++ Python/reduce.py
@@ -0,0 +1,18 @@
+# https://www.hackerrank.com/challenges/reduce-function/problem
+
+import operator
+from fractions import Fraction
+from functools import reduce
+
+
+def product(fracs):
+    t = reduce(operator.mul, fracs)  # complete this line with a reduce statement
+    return t.numerator, t.denominator
+
+
+if __name__ == '__main__':
+    fracs = []
+    for _ in range(int(input())):
+        fracs.append(Fraction(*map(int, input().split())))
+    result = product(fracs)
+    print(*result)
