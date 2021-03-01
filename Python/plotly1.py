fce611dc80e20aa1bc2e5cbb85748cce62073477
VIJAYA KUMARI YERUVA <VIJAYAYERUVA@users.noreply.github.com>
2021-02-28 23:36:29 -0600

    Day74 of # 100DaysOfCode

---
+++ Python/plotly1.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import plotly.offline as pyo
+
+import numpy as np
+
+# create fake data:
+df = pd.DataFrame(np.random.randn(100, 4), columns='A B C D'.split())
+pyo.plot([{
+    'x': df.index,
+    'y': df[col],
+    'name': col
+} for col in df.columns], filename='data/plotlyPlots/plotly1.html')
