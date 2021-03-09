eb436ba77885a23e049318b34d49dab2335d0109
VIJAYA KUMARI YERUVA <VIJAYAYERUVA@users.noreply.github.com>
2021-03-08 23:35:51 -0600

    Day82 of # 100DaysOfCode

---
+++ Python/plotly9.py
@@ -0,0 +1,6 @@
+import plotly.express as px
+import plotly.offline as pyo
+
+df = px.data.tips()
+fig = px.pie(df, values='tip', names='day', color_discrete_sequence=px.colors.sequential.RdBu)
+pyo.plot(fig, filename='data/plotlyPlots/plotly9.html')
