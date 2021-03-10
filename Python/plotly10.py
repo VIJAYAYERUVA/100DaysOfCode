aad20b66014df4f666f31fbbb1ff3db5646d2c40
VIJAYA KUMARI YERUVA <VIJAYAYERUVA@users.noreply.github.com>
2021-03-09 23:35:21 -0600

    Day83 of # 100DaysOfCode

---
+++ Python/plotly10.py
@@ -0,0 +1,8 @@
+import plotly.express as px
+import plotly.offline as pyo
+
+df = px.data.tips()
+
+fig = px.box(df, x="day", y="total_bill", color="smoker")
+fig.update_traces(quartilemethod="exclusive")  # or "inclusive", or "linear" by default
+pyo.plot(fig, filename='data/plotlyPlots/plotly10.html')
