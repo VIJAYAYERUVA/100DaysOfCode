b66d82adf86145882388cf84ee9b54dbe9232f43
VIJAYA KUMARI YERUVA <VIJAYAYERUVA@users.noreply.github.com>
2021-03-11 23:53:31 -0600

    Day85 of # 100DaysOfCode

---
+++ Python/plotly12.py
@@ -0,0 +1,7 @@
+import plotly.express as px
+import plotly.offline as pyo
+
+df = px.data.gapminder()
+fig = px.scatter(df.query("year==2007"), x="gdpPercap", y="lifeExp", size="pop", color="continent",
+                 hover_name="country", log_x=True, size_max=60)
+pyo.plot(fig, filename='data/plotlyPlots/plotly12.html')
