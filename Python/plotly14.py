92e1dff76b661f718e971df0a4afc3eed97d7d7e
VIJAYA KUMARI YERUVA <VIJAYAYERUVA@users.noreply.github.com>
2021-03-14 03:12:06 -0500

    Day87 of # 100DaysOfCode

---
+++ Python/plotly14.py
@@ -0,0 +1,7 @@
+import plotly.express as px
+import plotly.offline as pyo
+
+df = px.data.iris()
+features = ["sepal_width", "sepal_length", "petal_width", "petal_length"]
+fig = px.scatter_matrix(df, dimensions=features, color="species")
+pyo.plot(fig, filename='data/plotlyPlots/plotly14.html')
